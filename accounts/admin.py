cec30ac81c28c2954600d8ad10e910cdb2ae985e
ES, Choi <69956156+escape1001@users.noreply.github.com>
2024-04-03 21:28:09 +0900

    :wrench: accounts 어드민 커스텀
    - joinedCrew, joinedRace는 User 밑에서 inline으로 확인. 메뉴에선 숨김.
    - record 총합 User 밑에서 볼 수 있도록 추가

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,8 +1,35 @@
 from django.contrib import admin
+from django.db.models import Sum
 from .models import LevelStep, JoinedCrew, JoinedRace, Record, CustomUser
 
+
+class JoinedCrewInline(admin.TabularInline):
+    model = JoinedCrew
+    extra = 0
+    readonly_fields = ("created_at", "updated_at")
+
+
+class JoinedRaceInline(admin.TabularInline):
+    model = JoinedRace
+    extra = 0
+    readonly_fields = ("created_at", "updated_at")
+    
+
+class RecordInline(admin.TabularInline):
+    model = Record
+    extra = 0
+
+
+class CustomUserAdmin(admin.ModelAdmin):
+    list_display = ("username", "email", "user_type", "is_staff", "total_distance")
+    inlines = [JoinedCrewInline, JoinedRaceInline, RecordInline]
+    readonly_fields = ("total_distance",)
+
+    def total_distance(self, obj):
+        return obj.records.aggregate(total_distance=Sum('distance'))['total_distance'] or 0
+    total_distance.short_description = 'Total Distance'
+
+
 admin.site.register(LevelStep)
-admin.site.register(JoinedCrew)
-admin.site.register(JoinedRace)
 admin.site.register(Record)
-admin.site.register(CustomUser)
+admin.site.register(CustomUser, CustomUserAdmin)
