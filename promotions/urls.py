7b0ec0441e5ba17d86ad8d2dc78f0394c0d6d7dc
ES, Choi <69956156+escape1001@users.noreply.github.com>
2024-04-09 11:54:01 +0900

    :sparkles: 프로모션, 프로모션 post API fbv 작성 (GET)

--- promotions/urls.py
+++ promotions/urls.py
@@ -1,4 +1,7 @@
 from django.urls import path
+from . import views
 
 urlpatterns = [
+    path("", views.promotion_main, name="promotion_main"),
+    path("post/", views.promotion_post, name="promotion_post"),
 ]
--- promotions/views.py
+++ promotions/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+from .models import Promotion, PromotionArticle
+from .serializers import PromotionSerializer, PromotionArticleSerializer
 
-# Create your views here.
+
+@api_view(['GET'])
+def promotion_main(request):
+    # is_show가 true인 Promotion만 가져오기
+    promotions = Promotion.objects.filter(is_show=True)
+    serializer = PromotionSerializer(promotions, many=True)
+    return Response(serializer.data)
+
+
+@api_view(['GET'])
+def promotion_post(request):
+    # is_show가 true인 Promotion Article 최근 3개만 가져오기
+    promotion_articles = PromotionArticle.objects.filter(is_show=True).order_by('-updated_at')[:3]
+    serializer = PromotionArticleSerializer(promotion_articles, many=True)
+    return Response(serializer.data)
