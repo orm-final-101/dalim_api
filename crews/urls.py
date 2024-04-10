d219320b168588856e7a5877288968fa990e56a1
jiminkyung <jimiiwhatttttttttt@gmail.com>
2024-04-11 00:11:50 +0900

    🚑  crews url 수정
    - `crews/crews/` 오류 수정

--- crews/urls.py
+++ crews/urls.py
@@ -6,12 +6,12 @@ from . import views
 app_name = "crews"
 
 router = DefaultRouter()
-router.register("crews", views.PublicCrewViewSet, basename="public_crew")
-router.register("manage/crews", views.ManagerCrewViewSet, basename="manage_crew")
-router.register("crews/(?P<crew_id>\\d+)/reviews", views.CrewReviewViewSet, basename="crewreview")
-router.register("manage/crews/(?P<crew_id>\\d+)/members", views.CrewMemberViewSet, basename="joinedcrew")
+router.register("", views.PublicCrewViewSet, basename="public_crew")
+router.register("manage/", views.ManagerCrewViewSet, basename="manage_crew")
+router.register("(?P<crew_id>\\d+)/reviews", views.CrewReviewViewSet, basename="crewreview")
+router.register("manage/(?P<crew_id>\\d+)/members", views.CrewMemberViewSet, basename="joinedcrew")
 
 urlpatterns = [
     path("", include(router.urls)),
-    path("crews/popular/", views.PublicCrewViewSet.as_view({"get": "popular"}), name="crew-popular"),
+    path("popular/", views.PublicCrewViewSet.as_view({"get": "popular"}), name="crew-popular"),
 ]
