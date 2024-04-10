aae68547fb5e93968317112e4dc68a25f4d37f8e
refelim <idle52moments@gmail.com>
2024-04-10 12:42:10 +0900

    🔥 views.py 수정

--- boards/views.py
+++ boards/views.py
@@ -1,17 +1,14 @@
 from django.http import JsonResponse
 from django.shortcuts import get_object_or_404
-#from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework.pagination import PageNumberPagination
 from rest_framework.response import Response
 from rest_framework.permissions import AllowAny
 from rest_framework.decorators import permission_classes#,api_view
+from rest_framework.permissions import AllowAny
 from rest_framework import viewsets
 from .models import PostClassification, Category, Post, Comment, Like
 from .serializers import PostClassificationSerializer, CategorySerializer, PostSerializer, CommentSerializer
-from django.http import JsonResponse
-from django.shortcuts import get_object_or_404
-from rest_framework.permissions import AllowAny
-from rest_framework.decorators import permission_classes#, api_view
+
 
 class PostClassificationViewSet(viewsets.ModelViewSet):
     
