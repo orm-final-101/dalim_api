18a43cb3230583eaf968f0b8b2bbc9aa406f3337
northeast23 <ysyu099@example.com>
2024-04-10 11:00:23 +0100

    feature/#60 :wrench: 대회목록조회/GET

--- races/views.py
+++ races/views.py
@@ -1,33 +1,44 @@
-from django.db.models import Count, Q
-from accounts.models import JoinedCrew
-from .models import Race, RaceReview, RaceFavorite
-from rest_framework import status
-from rest_framework.decorators import api_view #, permission_classes
+# from django.db.models import Count, Q
+# from accounts.models import JoinedCrew
+# from .models import Race, RaceReview, RaceFavorite
+# from rest_framework import status
+# from rest_framework.decorators import api_view #, permission_classes
+# from rest_framework.response import Response
+# from rest_framework.permissions import IsAuthenticated
+# # from .permissions import IsCrewOwner, IsCrewAdmin, IsCrewMemberOrQuit
+# from .serializers import RaceListSerializer
+
+# # from django.shortcuts import get_object_or_404
+
+from rest_framework.decorators import api_view
 from rest_framework.response import Response
-from rest_framework.permissions import IsAuthenticated
-# from .permissions import IsCrewOwner, IsCrewAdmin, IsCrewMemberOrQuit
+from django.db.models import Q
 from .serializers import RaceListSerializer
-from config.constants import COURSE_CHOICES
-# from django.shortcuts import get_object_or_404
+from .models import Race 
+from datetime import date
 
 
 # 대회 목록 조회
 @api_view(["GET"])
 def race_list(request):
     races = Race.objects.all()
-    search_keyword = request.GET.get("search", "")
-    search_race_status = request.GET.get("search", "")
     
-    # if search_keyword:
-    #     races = races.filter(
-    #         Q(title__icontains=search_keyword) |
-    #         Q(description__icontains=search_keyword)
-    #     )
+    search_keyword = request.GET.get("search", "")
+    if search_keyword:
+        races = races.filter(
+            Q(title__icontains=search_keyword) |
+            Q(description__icontains=search_keyword)
+        )
 
-    # if selected_courses:
-    #     selected_courses = [course[0] for course in COURSE_CHOICES if course[0] in COURSE_CHOICES]
-    #     for course in COURSE_CHOICES:
-    #         courses = courses.filter(COURSE_CHOICES=course)
+    search_reg_status = request.GET.get("reg_status", "")
+    today = date.today()
+    if search_reg_status == "접수예정":
+        races = races.filter(reg_start_date__gt=today)
+    elif search_reg_status == "접수중":
+        races = races.filter(reg_start_date__lte=today, reg_end_date__gte=today)
+    elif search_reg_status == "접수마감":
+        races = races.filter(reg_end_date__lt=today)
+
 
     serializer = RaceListSerializer(races, many=True, context={"request": request})
     return Response(serializer.data)
