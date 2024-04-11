c51746d82bccff8914e5b85ee9e6f81aceb65db1
jiminkyung <jimiiwhatttttttttt@gmail.com>
2024-04-11 10:36:41 +0900

    🎨 간단한 코드 정리

--- crews/models.py
+++ crews/models.py
@@ -37,7 +37,7 @@ class CrewFavorite(models.Model):
 
 
 class CrewReview(models.Model):
-    crew = models.ForeignKey(Crew, related_name="reviews", on_delete=models.CASCADE)
+    crew = models.ForeignKey(Crew, on_delete=models.CASCADE, related_name="reviews")
     author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name="reviewd_crews")
     contents = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
