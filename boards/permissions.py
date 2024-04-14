bec4760561b00b472b2770b571c1a5a45dfc4d0a
refelim <idle52moments@gmail.com>
2024-04-15 01:55:02 +0900

    :bulb: permissions.py 주석 달기

--- boards/permissions.py
+++ boards/permissions.py
@@ -10,6 +10,7 @@ class IsAuthorOrReadOnly(permissions.BasePermission):
         # 쓰기 권한은 작성자에게만 허용
         return obj.author == request.user
 
+# 일반 사용자는 post_classification의 event, notice 글 작성 불가합니다.
 class IsStaffOrGeneralClassification(permissions.BasePermission):
     def has_permission(self, request, view):
         if request.method == 'POST':
