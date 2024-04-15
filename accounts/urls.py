dd6a1708fbaf3c875f6260e47b37c6e907b6f47d
ES, Choi <69956156+escape1001@users.noreply.github.com>
2024-04-16 00:48:43 +0900

    :wrench: router 하나로 수정

--- accounts/urls.py
+++ accounts/urls.py
@@ -5,21 +5,21 @@ from dj_rest_auth.views import LoginView, LogoutView
 from . import views
 
 
-record_router = DefaultRouter()
-record_router.register("", views.RecordViewSet, basename="record")
+app_name = "accounts"
 
-race_router = DefaultRouter()
-race_router.register("", views.RaceViewSet, basename="joined_race")
+router = DefaultRouter()
+router.register("mypage/record", views.RecordViewSet, basename="record")
+router.register("mypage/race", views.RaceViewSet, basename="joined_race")
+
 
 urlpatterns = [
+    path("", include(router.urls)),
     path("login/", LoginView.as_view(), name="account_login"),
     path("logout/", LogoutView.as_view(), name="account_logout"),
     path("signup/", views.CustomRegisterView.as_view(), name="account_signup"),
     path("token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
     path("mypage/info/", views.mypage_info),
     path("mypage/crew/", views.mypage_crew),
-    path("mypage/record/", include(record_router.urls)),
-    path("mypage/race/", include(race_router.urls)),
     path("<int:pk>/profile/", views.profile),
     path("mypage/favorites/", views.mypage_favorites),
 ]
