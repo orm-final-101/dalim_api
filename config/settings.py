b562d5be29b2aa0d6ed6b2880f8c9d56a2056852
ES, Choi <69956156+escape1001@users.noreply.github.com>
2024-04-16 17:08:05 +0900

    :bug: 배포 시 필요한 static_root 추가

--- config/settings.py
+++ config/settings.py
@@ -132,6 +132,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / "staticfiles"
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
